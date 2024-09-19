e06a2a3207cdfb007fe73065f492dc34c063266f
jsillero-ga <jsillerosalado@gmail.com>
2024-09-19 17:24:14 +0200

    First round of styles, now rebranded to Grave Robber

--- script/controller.js
+++ script/controller.js
@@ -1,16 +1,60 @@
+/* Init storage variable for time record ________________________________ */
+if (localStorage.getItem("easy") == null) {
+    localStorage.setItem("easy", "9999");
+    localStorage.setItem("mid", "9999");
+    localStorage.setItem("hard", "9999");
+}
 
-/* Setting element chached */
-const mineField = document.getElementById("minefield")
 
-/* Setting variables */
+/* Setting variables _____________________________________________________*/
 let board;
 let winstate = null;
+let clock;
 
-/* Setting event listeners  */
+/* Setting element chached ________________________________________________*/
+const mineField = document.getElementById("minefield");
+
+const auxGameInfo = document.getElementById("gameInfo");
+const timeDisplay = document.getElementById("timer");
+
+
+const winstateElement = document.getElementById("winstatescreen");
+const winstateMessageElement = document.getElementById("winstatemessage");
+const curentTimeSpan = document.getElementById("yourTime");
+const recordTimeSpan = document.getElementById("topTime");
+const newRecordMessage = document.getElementById("newrecordMessage");
+const recordMessage = document.getElementById("recordMessage");
+
+const settingButton = document.getElementById("btnSettings");
+const settingsElement = document.getElementById("settings");
+const settingCloseBtn = document.getElementById("closeSettings");
+
+const mainMenuElement = document.getElementById("mainMenu");
+
+const mainMenuButton = document.getElementById("mainMenuButton");
+
+const playButtons = document.getElementsByClassName("start");
+
+const radioElements = document.querySelectorAll("input[type='radio']");
+
+const flagElement = document.getElementById("flagElement");
+const flagCount = document.getElementById("flagCount");
+const totalMines = document.getElementById("totalMines");
+
+
+/* Setting event listeners _______________________________________________ */
 mineField.addEventListener("click", clickCell);
-mineField.addEventListener("contextmenu", (e) => {
+mineField.addEventListener("contextmenu", (e) => {//disable contextual menu on right click
     e.preventDefault();
 });
+
+mainMenuButton.addEventListener("click", mainMenu);
+
+settingButton.addEventListener("click", settingsMenu);
+
+settingCloseBtn.addEventListener("click", closeSettings);
+
+
 mineField.addEventListener("auxclick", (e) => {//put a flag item on secondary click
     let currentElement = e.target;
     if ((currentElement.classList.contains("cell") || currentElement.classList.contains("flag")) && winstate == null) {
@@ -23,25 +67,135 @@ mineField.addEventListener("auxclick", (e) => {//put a flag item on secondary cl
             currentElement.classList.remove("cell");
         }
     }
+    let count = flagCount.innerHTML = document.querySelectorAll("#minefield .flag").length;
+    if (count !== undefined) {
+        flagCount.innerHTML = count;
+    } else {
+        flagCount.innerHTML = 0;
+    }
+
     e.preventDefault();
 });
 
-function createMineField() {
+for (const button of playButtons) {
+    button.addEventListener("click", (e) => {
+        gameStart();//hide or show the needed options / menus elements
+        let difficulty = document.querySelector('input[name="difficulty"]:checked').value;
+        winstate = null;
+        switch (difficulty) {
+            case "easy":
+                createMineField(8, 1);
+                totalMines.innerHTML = "1";
+
+                break;
+            case "mid":
+                createMineField(10, 15);
+                totalMines.innerHTML = "15";
+
+                break;
+            case "hard":
+                createMineField(20, 75);
+                totalMines.innerHTML = "75";
+                break;
+            default:
+                break;
+        }
+
+    })
+
+}
+
+
+for (const radio of radioElements) {
+    radio.addEventListener("change", (e) => {
+        let target = e.target;
+        let idChosen = target.id.substring(0, target.id.length - 1);
+        document.getElementById(`${idChosen}1`).checked = true;
+        document.getElementById(`${idChosen}2`).checked = true;
+        document.getElementById(`${idChosen}3`).checked = true;
+
+    })
+}
+
+/* PROPER CODE___________________________________________________________ */
+
+function createMineField(size, mines) {
     mineField.innerHTML = "";//clear the element
-    board = new Board(6, 1);
+    board = new Board(size, mines);
     board.mineField.forEach((line, indexY) => {
-        let htmlLine = `<div class='line' >`;
+        let htmlLine = document.createElement("div"); //create the div elements which will countain lines of cells
+        htmlLine.setAttribute("class", "line");
+
         line.forEach((cell, indexX) => {
-            htmlLine += `<div class='cell' data-x='${indexX}' data-y='${indexY}'></div>`
+            let cellElement = document.createElement("div");
+            cellElement.setAttribute("class", "cell");
+            cellElement.setAttribute("data-x", indexX);
+            cellElement.setAttribute("data-y", indexY);
+            htmlLine.appendChild(cellElement);
         });
-        mineField.innerHTML += htmlLine + "</div>";
+        mineField.appendChild(htmlLine);
     });
 }
 
-createMineField();
+function gameEnd() {
+    if (winstate) {
+        winstateMessageElement.innerHTML = "Robbed them blind!";
+        recordMessage.classList.remove("hidden");
+        //set new record if there was one
+        let difficulty = document.querySelector('input[name="difficulty"]:checked').value;
+        let pastRecord = localStorage.getItem(difficulty);
+        let currentRecord = mins + "" + seconds;
+        if (currentRecord < pastRecord) {
+            localStorage.setItem(difficulty, currentRecord);
+            newRecordMessage.classList.remove("hidden");//if a new record is set the message is revealed
+        }
+
+        curentTimeSpan.innerHTML = ((mins.length = 1) ? mins = "0" + mins : mins) + ":" + seconds;
+
+        let recordmins = localStorage.getItem(difficulty).substring(0, 2);
+        let recordSecs = localStorage.getItem(difficulty).substring(2);
+
+        recordTimeSpan.innerHTML = `${recordmins}:${(recordSecs.length = 1) ? recordSecs = "0" + recordSecs : recordSecs}`;
+
+
+
+    } else {
+        winstateMessageElement.innerHTML = "You joined the corpses!";
+        recordMessage.classList.add("hidden");
+    }
+    winstateElement.classList.remove("hidden");
+    resetTime();
+}
+
+function gameStart() {
+    winstateElement.classList.add("hidden");
+    mainMenuElement.classList.add("hidden");
+    recordMessage.classList.add("hidden");
+    newRecordMessage.classList.add("hidden");
+
+
+    auxGameInfo.classList.remove("hidden");
+    mineField.classList.remove("hidden");
+    flagElement.classList.remove("hidden");
+    flagCount.innerHTML = 0;
+
+    time();
+    clock = window.setInterval(time, 999);
+}
+
+function mainMenu() {
+    mineField.classList.add("hidden");
+    winstateElement.classList.add("hidden");
+    auxGameInfo.classList.add("hidden");
+    flagElement.classList.add("hidden");
+
+
+    mainMenuElement.classList.remove("hidden");
+
+}
+
 
 function clickCell(e) {
-    let mouseclickType = e.buttons;
     let currentElement = e.target;
     if (currentElement.classList.contains("cell") && winstate == null) {
         let coordX = currentElement.dataset.x;
@@ -57,7 +211,12 @@ function clickCell(e) {
                 for (let suby = -1; suby < 2; suby++) {
                     //catch the error in case we try to access an non existent element
                     try {
-                        document.querySelector(`div[data-x="${(Number(coordX) + Number(subx))}"][data-y="${(Number(coordY) + Number(suby))}"]`).click();
+                        let nextCell = document.querySelector(`div.cell[data-x="${(Number(coordX) + Number(subx))}"][data-y="${(Number(coordY) + Number(suby))}"]`);
+                        if (nextCell) {
+                            nextCell.click();
+                        }
+                        //document.querySelector(`div.cell[data-x="${(Number(coordX) + Number(subx))}"][data-y="${(Number(coordY) + Number(suby))}"]`).click();
+
                     } catch (error) {
 
                     }
@@ -68,12 +227,51 @@ function clickCell(e) {
             currentElement.innerHTML = result;
         } else {
             currentElement.innerHTML = "M";
-            //TODO ADD LOOSE FUNCTION
+            winstate = false;
+            gameEnd();
         }
         //increases reveal count and checks for the wincon
         if (board.increaseRevealCount()) {
             winstate = true;
-            console.log("YOU WIN;");
+            gameEnd();
         };
     }
+}
+
+let seconds = 0;
+let mins = 0;
+
+/* Displays time, actualices every second,  */
+function time() {
+    seconds++
+    if (seconds > 59) {
+        seconds = 0;
+        mins++;
+    }
+    timeDisplay.innerHTML = `${(mins < 9 || mins == 0) ? "0" + mins : mins}:${(seconds < 10 || seconds == 0) ? seconds = "0" + seconds : seconds}`;
+}
+/* Resetea los valores de la hora */
+function resetTime() {
+    seconds = 0;
+    mins = 0;
+    clearInterval(clock);
+}
+
+function pauseTime() {
+    clearInterval(clock);
+}
+
+function resumeTime() {
+    clock = window.setInterval(time, 999);
+}
+
+function settingsMenu() {
+    if (winstate === null)
+        pauseTime();
+    settingsElement.classList.remove("hidden");
+}
+function closeSettings() {
+    if (winstate === null)
+        resumeTime();
+    settingsElement.classList.add("hidden");
 }
--- script/model.js
+++ script/model.js
@@ -76,3 +76,4 @@ class Board {
         return false;
     }
 }
+
